9be6732d527eb1a542e461a04e278a6fb35466a7
herbowicz <herbowicz@gmail.com>
2023-12-17 12:04:54 +0100

    api POST create

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,26 @@
+import { Api, StackContext, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
+    routes: {
+      "POST /notes": "packages/functions/src/create.main",
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  return {
+    api,
+  };
+}
